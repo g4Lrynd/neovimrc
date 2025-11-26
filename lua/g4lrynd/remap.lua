4cff90daa1bf8f21bcff3dda0fea2ef8d9aa9c0b
Zakariya Bouzid <zakariya.bouzid@outlook.com>
2025-11-26 20:13:01 +0000

    Added luasnip and keymaps for better terminal usage

--- lua/g4lrynd/remap.lua
+++ lua/g4lrynd/remap.lua
@@ -1,5 +1,7 @@
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
@@ -7,7 +9,6 @@ vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 vim.keymap.set("i", "<C-c>", "<Esc>")
-
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 
@@ -16,3 +17,22 @@ vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
+
+vim.api.nvim_create_autocmd('TermOpen', {
+  pattern = '*',
+  callback = function()
+    local opts = { buffer = 0, noremap = true, silent = true }
+
+    -- Alt+c: leave terminal-insert without closing the pane
+    vim.keymap.set('t', '<A-c>', [[<C-\><C-n>]], opts)
+
+    -- Make Ctrl+w run window commands from a terminal
+    vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+
+    -- Optional: quick nav from terminal
+    vim.keymap.set('t', '<C-h>', [[<C-\><C-n><C-w>h]], opts)
+    vim.keymap.set('t', '<C-j>', [[<C-\><C-n><C-w>j]], opts)
+    vim.keymap.set('t', '<C-k>', [[<C-\><C-n><C-w>k]], opts)
+    vim.keymap.set('t', '<C-l>', [[<C-\><C-n><C-w>l]], opts)
+  end,
+})
